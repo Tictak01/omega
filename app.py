180f46069c14c5a4feb0392bf3e093421bf2189b
Nephus <nephus.cypher@gmail.com>
2020-02-14 05:25:45 -0700

    Common Layout

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def index():
     test_packages = ['p1', 'p2', 'p3']
     return flask.render_template('index.html', packages=test_packages)
 
+@app.route('/about')
+def about():
+    return flask.render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
