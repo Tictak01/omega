02d99b967cd484a6697ffdcec3d417eefb0693f5
Nephus <nephus.cypher@gmail.com>
2020-02-14 05:33:49 -0700

    Layout cleanup

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     test_packages = ['p1', 'p2', 'p3']
-    return flask.render_template('index.html', packages=test_packages)
+    return flask.render_template('home/index.html', packages=test_packages)
 
 @app.route('/about')
 def about():
-    return flask.render_template('about.html')
+    return flask.render_template('home/about.html')
 
 
 if __name__ == '__main__':
