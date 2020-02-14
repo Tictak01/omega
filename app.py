59256f1706eef96df418ac476e0b753c8bc945d9
Robert Rowland <robert.rowland@attainia.com>
2020-02-14 08:53:04 -0700

    Added dictionary

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 import flask
 app = flask.Flask(__name__)
 
-
+def get_details():
+    items = [{'name': 'details1', 'version':'1'}, {'name': 'butt', 'version':'0.95'}, {'Wut': 'sup'}]
+    return items
 @app.route('/')
 def index():
     test_packages = ['p1', 'p2', 'p3']
-    return flask.render_template('home/index.html', packages=test_packages)
+    return flask.render_template('home/index.html', packages=get_details())
 
 @app.route('/about')
 def about():
